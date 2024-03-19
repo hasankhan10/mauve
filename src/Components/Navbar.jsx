adbad1fbe452776381138929c74075d7d8d85295
mehedi <darkhasan10@gmail.com>
2024-03-19 22:41:47 +0530

    Create home page

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <div className='navbar absolute w-[100%] h-[50px] top-7 flex justify-start pl-16 space-x-96 z-50'>
-        <nav className='logo h-full w-fit'>
+        <nav className='logo h-full w-fit hover:scale-105 duration-200'>
           <img className=' h-full w-full rounded-md' src="https://static.wixstatic.com/media/5edfa9_ee63e8792bbc4428ae15f05ddaaadb33~mv2.png/v1/fill/w_64,h_74,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/mi.png" alt="" />
         </nav>
         <nav className=' h-full w-[43%] backdrop:blur-3xl rounded-lg overflow-hidden flex justify-center shadow-lg shadow-gray-500 hover:shadow-gray-600 duration-200'>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,22 @@
+
+import { TypeAnimation } from 'react-type-animation';
 function Home() {
   return (
-    <div className="home w-screen h-screen">
+    <div className="home w-screen h-screen flex justify-end">
+        <div className="welcome w-[50%] h-full flex flex-col justify-center place-items-center">
+            <h1 className=" text-8xl text-center h-[30%] opacity-95 text-gray-800">
+              <TypeAnimation
+                sequence={["Wellcome to Mauve Interio"]}
+                cursor={false}
+                speed={20}
+              />
+            </h1>
+            <p className=" text-2xl text-center font-mono font-semibold opacity-95">Interior Design for Great Homes and Working Space</p>
+            <p className="text-center text-3xl mt-6 p-2 opacity-80 font-semibold">“It’s not what you look at that matters, it’s what you see” - Henry David Thoreau
+            </p>
 
+            <button className=" w-56 h-14 bg-blue-800 text-white font-bold text-xl mt-8 rounded-md hover:scale-105 duration-300 hover:shadow-2xl shadow-black">Get in touch</button>
+        </div>
     </div>
   )
 }
