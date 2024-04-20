83d26af0df18fb6dae4fd6b396abf80213696f55
mehedi <darkhasan10@gmail.com>
2024-04-20 12:08:06 +0530

    add animation in about us page

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-
+import {motion} from "framer-motion"
 function AboutUs() {
   return (
     <div className='about w-screen h-screen flex justify-center place-items-center'>
-        <section className=' sm:w-[75%] sm:h-[80%] flex sm:gap-10 sm:flex-row flex-col justify-center place-items-center'>
+        <motion.section animate={{opacity:1}} initial={{opacity:0}} transition={{duration:1}} className=' sm:w-[75%] sm:h-[80%] flex sm:gap-10 sm:flex-row flex-col justify-center place-items-center'>
             <div className=' sm:w-[45%] w-full sm:h-fit h-full sm:backdrop-blur-3xl backdrop-blur-md flex flex-col gap-7 text-center p-9 rounded-xl'>
                 <h1 className=' sm:text-5xl text-3xl font-bold text-gray-900'>ALL ABOUT US</h1>
                 <p className=' text-xl'>Mauve interio is here to make your design dream to a reality. Whatever you’ve envisioned for your home or workspace, we are here to get it done. Since 2019, our professional, creative team has been designing the spaces that are most important to our clients. Let us lead the way to an outcome you’ll absolutely love.
@@ -13,8 +13,8 @@ function AboutUs() {
             <NavLink to={"/contact"}>
               <button className="sm:hidden w-56 h-14 bg-blue-800 text-white font-bold text-xl mt-8 rounded-md hover:scale-105 duration-300 hover:shadow-2xl shadow-black">Get in touch</button>
             </NavLink>
-            <div className='abpi w-1/2 h-[90%] rounded-xl'></div>
-        </section>
+            <motion.div animate={{x:0}} initial={{x:200}} transition={{duration:1}} className='abpi w-1/2 h-[90%] rounded-xl'></motion.div>
+        </motion.section>
     </div>
   )
 }
