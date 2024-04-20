e9ad9c64f8db215d7c1017827a79e844527af6cd
mehedi <darkhasan10@gmail.com>
2024-04-20 11:50:41 +0530

    add animation in navbar and home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
-
 import { NavLink } from 'react-router-dom';
 import { TypeAnimation } from 'react-type-animation';
+import {motion} from "framer-motion"
 function Home() {
   return (
     <div className="home w-screen h-screen flex sm:justify-end justify-center">
@@ -12,13 +12,13 @@ function Home() {
                 speed={15}
               />
             </h1>
-            <p className=" text-2xl text-center font-mono font-semibold opacity-95">Interior Design for Great Homes and Working Space</p>
-            <p className=" text-center text-3xl mt-6 p-2 opacity-80 font-semibold">“It’s not what you look at that matters, it’s what you see” - Henry David Thoreau
-            </p>
+            <motion.p animate={{x:0,opacity:1}} initial={{x:-300,opacity:0}} transition={{duration:2}} className=" text-2xl text-center font-mono font-semibold opacity-95">Interior Design for Great Homes and Working Space</motion.p>
+            <motion.p animate={{x:0,opacity:1}} initial={{x:300,opacity:0}} transition={{duration:2}} className=" text-center text-3xl mt-6 p-2 opacity-80 font-semibold">“It’s not what you look at that matters, it’s what you see” - Henry David Thoreau
+            </motion.p>
 
-            <NavLink to={"/contact"} className={" z-50"}>
+            <motion.NavLink animate={{y:0,opacity:1}} initial={{y:100,opacity:0}} transition={{duration:2}} to={"/contact"} className={" z-50"}>
               <button className=" w-56 h-14 bg-blue-800 text-white font-bold text-xl mt-8 rounded-md hover:scale-105 duration-300 hover:shadow-2xl shadow-black">Get in touch</button>
-            </NavLink>
+            </motion.NavLink>
         </div>
     </div>
   )
